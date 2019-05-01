a6d5ad03cd993fc0c9a5f4279da9fb2d63a48451
hannasgithub <budayevah@gmail.com>
2019-05-01 16:31:52 -0400

    add a colex function

--- myproject.R
+++ myproject.R
@@ -2,4 +2,7 @@ data <- iris
 plot(x=irirs$Sepal.length, y=iris$Sepal.Width)
 plot(x=iris$Sepal.Length, y=iris$Petal.Width)
 #hist
+complex_function <- function(input) {
+  print("we are doing complex things here!")
+}
 
